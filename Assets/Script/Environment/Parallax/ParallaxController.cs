15ef4794d9b61ce18d07c4608067f5bfad6dae39
zsl2021533013 <185600557@qq.com>
2022-10-17 11:15:16 +0800

    初步整改

--- Assets/Script/Environment/Parallax/ParallaxController.cs
+++ Assets/Script/Environment/Parallax/ParallaxController.cs
@@ -4,40 +4,40 @@ namespace Environment.Parallax
 {
     public class ParallaxController : MonoBehaviour
     {
-        //private Camera _cam;
-        //private float _cameraPositionX;
+        private Camera _cam;
+        private float _cameraPositionX;
 
-        //private Vector3 _newPosition;
-        ////private float _startX;
-        ////private float _startY;
-        ////private float _startZ;
+        private Vector3 _newPosition;
+        private float _startX;
+        private float _startY;
+        private float _startZ;
 
-        //private float Travel => _cam.transform.position.x - _cameraPositionX;
+        private float Travel => _cam.transform.position.x - _cameraPositionX;
 
-        ////private float DistanceFromSubject => _startZ;
+        private float DistanceFromSubject => _startZ; // _startZ 为负数时仍有意义
 
-        ////private float ClippingPlane =>
-        ////    (_cam.transform.position.z + (DistanceFromSubject > 0 ? _cam.farClipPlane : _cam.nearClipPlane));
-        ////private float ParallaxFactor => DistanceFromSubject / Mathf.Abs(ClippingPlane);
+        private float ClippingPlane =>
+            (_cam.transform.position.z + (DistanceFromSubject > 0 ? _cam.farClipPlane : _cam.nearClipPlane));
+        private float ParallaxFactor => DistanceFromSubject / Mathf.Abs(ClippingPlane);
 
-        //private void Awake()
-        //{
-        //    _cam = Camera.main;
-        //    _cameraPositionX = _cam.transform.position.x;
+        private void Awake()
+        {
+            _cam = Camera.main;
+            _cameraPositionX = _cam.transform.position.x;
 
-        //    Vector3 position = transform.position;
-        //    //_startX = position.x;
-        //    //_startY = position.y;
-        //    //_startZ = position.z;
-        //}
+            Vector3 position = transform.position;
+            _startX = position.x;
+            _startY = position.y;
+            _startZ = position.z;
+        }
 
-        //private void FixedUpdate()
-        //{
-        //    //_newPosition.Set(_startX + Travel * ParallaxFactor, _startY, _startZ);
-        //    //transform.position = _newPosition;
-        //}
+        private void FixedUpdate()
+        {
+            _newPosition.Set(_startX + Travel * ParallaxFactor, _startY, _startZ);
+            transform.position = _newPosition;
+        }
 
-        [Header("画布相较于相机的移动速度")] public Vector2 speed;
+        /*[Header("画布相较于相机的移动速度")] public Vector2 speed;
         [Header("纵轴循环")] public bool infiniteHorizontal;
         [Header("横轴循环")] public bool infiniteVertical;
 
@@ -79,6 +79,6 @@ namespace Environment.Parallax
                 }
             }
 
-        }
+        }*/
     }
 }
