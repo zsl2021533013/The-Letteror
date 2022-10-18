69f29b42c94ba81a8b5075e017591247c57fb384
zsl2021533013 <185600557@qq.com>
2022-10-18 09:27:24 +0800

    第二关装饰初步完成

--- Assets/Script/Environment/Parallax/ParallaxController.cs
+++ Assets/Script/Environment/Parallax/ParallaxController.cs
@@ -1,9 +1,16 @@
+using System;
 using UnityEngine;
 
 namespace Environment.Parallax
 {
     public class ParallaxController : MonoBehaviour
     {
+        /*[Header("Constrain")]
+        [SerializeField]
+        private float constraintXMin;
+        [SerializeField]
+        private float constraintXMax;*/
+    
         private Camera _cam;
         private float _cameraPositionX;
 
@@ -33,52 +40,16 @@ namespace Environment.Parallax
 
         private void FixedUpdate()
         {
+            /*float targetX = Mathf.Clamp(_startX + Travel * ParallaxFactor, constraintXMin, constraintXMax);*/
             _newPosition.Set(_startX + Travel * ParallaxFactor, _startY, _startZ);
             transform.position = _newPosition;
         }
 
-        /*[Header("画布相较于相机的移动速度")] public Vector2 speed;
-        [Header("纵轴循环")] public bool infiniteHorizontal;
-        [Header("横轴循环")] public bool infiniteVertical;
-
-        private Transform cameraTransform;
-        private Vector3 lastCameraPosition;
-        private float textureUnitSizeX;
-        private float textureUnitSizeY;
-
-        private void Awake()
+        /*private void OnDrawGizmosSelected()
         {
-            cameraTransform = Camera.main.transform;
-            lastCameraPosition = cameraTransform.position;
-            Sprite sprite = GetComponent<SpriteRenderer>().sprite;
-            Texture2D texture = sprite.texture;
-            textureUnitSizeX = texture.width / sprite.pixelsPerUnit;
-            textureUnitSizeY = texture.height / sprite.pixelsPerUnit;
-        }
-
-        private void FixedUpdate()
-        {
-            Vector3 deltaMovement = cameraTransform.position - lastCameraPosition;
-            transform.position += new Vector3(deltaMovement.x * speed.x, deltaMovement.y * speed.y, deltaMovement.z);
-            lastCameraPosition = cameraTransform.position;
-
-            if (infiniteVertical) 
-            {
-                if (Mathf.Abs(cameraTransform.position.x - transform.position.x) >= textureUnitSizeX)
-                {
-                    float offsetPositionX = (cameraTransform.position.x - transform.position.x) % textureUnitSizeX;
-                    transform.position = new Vector3(cameraTransform.position.x + offsetPositionX, transform.position.y);
-                }
-            }
-            if (infiniteHorizontal) 
-            {
-                if (Mathf.Abs(cameraTransform.position.y - transform.position.y) >= textureUnitSizeY)
-                {
-                    float offsetPositionY = (cameraTransform.position.y - transform.position.y) % textureUnitSizeY;
-                    transform.position = new Vector3(transform.position.x, cameraTransform.position.y + offsetPositionY);
-                }
-            }
-
+            Vector2 tmp = new Vector2(constraintXMin, 1);
+            Gizmos.color = Color.yellow;
+            Gizmos.DrawSphere(tmp, 0.5f);
         }*/
     }
 }
