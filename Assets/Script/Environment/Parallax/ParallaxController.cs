628890705b97ec4dcbe2ac97817abb0abb3eca1a
zsl2021533013 <185600557@qq.com>
2022-10-19 19:04:03 +0800

    洞穴初步搭建

--- Assets/Script/Environment/Parallax/ParallaxController.cs
+++ Assets/Script/Environment/Parallax/ParallaxController.cs
@@ -6,21 +6,18 @@ namespace Environment.Parallax
 {
     public class ParallaxController : MonoBehaviour
     {
-        /*[Header("Constrain")]
-        [SerializeField]
-        private float constraintXMin;
-        [SerializeField]
-        private float constraintXMax;*/
-        
+        public float _cameraStartPositionX;
+        public float minX;
+        public float maxX;
+
         private Camera _cam;
-        private float _cameraPositionX;
 
         private Vector3 _newPosition;
         private float _startX;
         private float _startY;
         private float _startZ;
 
-        private float Travel => _cam.transform.position.x - _cameraPositionX;
+        private float Travel => _cam.transform.position.x - _cameraStartPositionX;
 
         private float DistanceFromSubject => _startZ; // _startZ 为负数时仍有意义
 
@@ -31,7 +28,6 @@ namespace Environment.Parallax
         private void Awake()
         {
             _cam = Camera.main;
-            _cameraPositionX = _cam.transform.position.x;
 
             Vector3 position = transform.position;
             _startX = position.x;
@@ -46,22 +42,29 @@ namespace Environment.Parallax
 
         private void FixedUpdate()
         {
-            /*float targetX = Mathf.Clamp(_startX + Travel * ParallaxFactor, constraintXMin, constraintXMax);*/
-            _newPosition.Set(_startX + Travel * ParallaxFactor, _startY, _startZ);
+            float targetX = Mathf.Clamp(_startX + Travel * ParallaxFactor, minX, maxX);
+            _newPosition.Set(targetX, _startY, _startZ);
             transform.position = _newPosition;
         }
 
         public void UpdateParallaxController()
         {
             _cam = Camera.main;
-            _cameraPositionX = _cam.transform.position.x;
+            _cameraStartPositionX = _cam.transform.position.x;
         }
 
-        /*private void OnDrawGizmosSelected()
+        private void OnDrawGizmosSelected()
         {
-            Vector2 tmp = new Vector2(constraintXMin, 1);
-            Gizmos.color = Color.yellow;
-            Gizmos.DrawSphere(tmp, 0.5f);
-        }*/
+            Vector2 tmp = new Vector2(_cameraStartPositionX, transform.position.y);
+            Gizmos.color = Color.red;
+            
+            Gizmos.DrawWireSphere(tmp, 1f);
+            
+            tmp.Set(minX, transform.position.y);
+            Gizmos.DrawWireSphere(tmp, 1f);
+            
+            tmp.Set(maxX, transform.position.y);
+            Gizmos.DrawWireSphere(tmp, 1f);
+        }
     }
 }
