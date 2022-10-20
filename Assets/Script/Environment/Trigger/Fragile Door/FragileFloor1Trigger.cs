19cabe7de96e7bfb00ba3e77871f7ce6f9ccbc89
zsl2021533013 <185600557@qq.com>
2022-10-20 22:52:41 +0800

    机关的一些补全

---
+++ Assets/Script/Environment/Trigger/Fragile Door/FragileFloor1Trigger.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+using Character.Player.FSM.Player_State.Sub_State.Ability_State.Attack_State.Special_Attack;
+using Character.Player.Manager;
+using Environment.Trigger.Base;
+using UnityEngine;
+
+namespace Environment.Trigger
+{
+    public class FragileFloor1Trigger : TriggerBase
+    {
+        private BoxCollider2D _boxCollider2D;
+        private Animator _animator;
+        private static readonly int Disappear = Animator.StringToHash("disappear");
+
+        private void Awake()
+        {
+            _boxCollider2D = GetComponent<BoxCollider2D>();
+            _animator = GetComponent<Animator>();
+        }
+
+        public override void Interact(PlayerManager manager)
+        {
+            base.Interact(manager);
+
+            _animator.SetBool(Disappear, true);
+            StopAllCoroutines();
+            StartCoroutine(ResetAnimation());
+        }
+
+        IEnumerator ResetAnimation()
+        {
+            yield return new WaitForSeconds(5f);
+            _animator.SetBool(Disappear, false);
+        }
+    }
+}
---
+++ Assets/Script/Environment/Trigger/Fragile Door/FragileFloor2Trigger.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+using Character.Player.FSM.Player_State.Sub_State.Ability_State.Attack_State.Special_Attack;
+using Character.Player.Manager;
+using Environment.Trigger.Base;
+using UnityEngine;
+
+namespace Environment.Trigger
+{
+    public class FragileFloor2Trigger : TriggerBase
+    {
+        private BoxCollider2D _boxCollider2D;
+        private Animator _animator;
+        private static readonly int Disappear = Animator.StringToHash("disappear");
+
+        private void Awake()
+        {
+            _boxCollider2D = GetComponent<BoxCollider2D>();
+            _animator = GetComponent<Animator>();
+        }
+
+        public override void Interact(PlayerManager manager)
+        {
+            base.Interact(manager);
+
+            if (manager.StateMachine.CurrentState is PlayerSpecialDownwardsAttack2State or PlayerSpecialUpwardsAttackState)
+            {
+                _animator.SetBool(Disappear, true);
+            }
+        }
+
+        public void OnAnimationFinish()
+        {
+            Destroy(gameObject);
+        }
+    }
+}
